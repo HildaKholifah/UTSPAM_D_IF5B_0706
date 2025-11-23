c93a3d2b7972c696d7490861f12d25720daf3393
HildaKholifah <rahmadanikholifahh@gmail.com>
2025-11-23 10:36:21 +0700

    Menambahkan file model obat.dart

---
+++ lib/data/model/obat.dart
@@ -0,0 +1,26 @@
+class Obat {
+  int? id;
+  String nama;
+  String kategori;
+  int harga;
+
+  Obat({
+    this.id,
+    required this.nama,
+    required this.kategori,
+    required this.harga,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'nama': nama, 'kategori': kategori, 'harga': harga};
+  }
+
+  factory Obat.fromMap(Map<String, dynamic> map) {
+    return Obat(
+      id: map['id'],
+      nama: map['nama'],
+      kategori: map['kategori'],
+      harga: map['harga'],
+    );
+  }
+}
