ec2dfa487cbead96a787cd790fe22c004810b597
HildaKholifah <rahmadanikholifahh@gmail.com>
2025-11-23 08:14:02 +0700

    Membuat user_repository

---
+++ lib/data/repository/user_repository.dart
@@ -0,0 +1,40 @@
+import 'package:projectuts/data/db/user_dao.dart';
+import 'package:projectuts/data/model/user.dart';
+
+class UserRepository {
+  final _userDao = UserDao();
+
+  Future<void> register({
+    required String nama,
+    required String email,
+    required String alamat,
+    required String telepon,
+    required String username,
+    required String password,
+    required String gender,
+  }) async {
+    User user = User(
+      nama: nama,
+      email: email,
+      alamat: alamat,
+      telepon: telepon,
+      username: username,
+      password: password,
+      gender: gender,
+    );
+    await _userDao.insertUser(user);
+  }
+
+  Future<bool> login({
+    required String email,
+    required String password,
+  }) async {
+    User? user = await _userDao.getUserByEmail(email);
+    if (user == null) return false;
+    return user.password == password;
+  }
+
+  Future<User?> getUserByUsername(String username) async {
+    return await _userDao.getUserByUsername(username);
+  }
+}
